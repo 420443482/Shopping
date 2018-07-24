68dcb0ffc9e2270e6075d28ae903e8cb67fbb65f
余陈 <420443482@qq.com>
2018-07-24 17:14:11 +0800

    wx2

--- application/wxxs/controller/Novel.php
+++ application/wxxs/controller/Novel.php
@@ -135,8 +135,9 @@ class Novel extends Controller
 //       echo "<pre>";
 //        print_r($page[0][0]);
 //        exit;
-      
-        echo json_encode(array('code'=>1,'data'=>strip_tags($data[0][0])));
+       $data=str_replace(array("&nbsp;","&amp;nbsp;","\t","\r\n","\r","\n"),array("","","","","",""),$data[0][0]);
+
+        echo json_encode(array('code'=>1,'data'=>strip_tags($data)));
         exit;
     }
 
