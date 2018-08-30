060bbe87e86da5805555cda4b493b6c3cc961446
余陈 <420443482@qq.com>
2018-08-30 16:28:42 +0800

    video

---
+++ application/wxxs/controller/Video.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\wxxs\controller;
+use app\common\model\User;
+use think\Controller;
+use think\Db;
+
+class Video extends Controller
+{
+   public function video_message(){
+       $type = isset($_REQUEST['type'])?$_REQUEST['type']:'';
+       if(empty($type))$this->error('请选择视频分类');
+       $data = Db::name('video')->where(array('video_type'=>$type))->select();
+       echo json_encode(array('code'=>1,'data'=>$data));
+   }
+}
+
