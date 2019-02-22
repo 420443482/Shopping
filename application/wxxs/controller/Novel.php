1ee82bbfb4a2bf621cc23898034325528bf525c5
余陈 <420443482@qq.com>
2019-02-22 09:36:03 +0800

    1

--- application/wxxs/controller/Novel.php
+++ application/wxxs/controller/Novel.php
@@ -47,6 +47,8 @@ class Novel extends Controller
         curl_setopt ( $ch, CURLOPT_URL, $url );
         curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, 1 );
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);//这个是重点。
+        curl_setopt($ch,CURLOPT_FOLLOWLOCATION,1);
+        
         $content = curl_exec ($ch);
         if ($content == FALSE) {
             echo "error:" . curl_error ( $ch );
